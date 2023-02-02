04eafce215fd69d42ad989b7c87fddb6c03a5299
Gingerbread Boy <mryanjacinto@gmail.com>
2023-02-02 15:08:11 +0800

    update elements

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// $('.brands-wrap').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 0,
+//     speed: 5000,
+//     pauseOnHover: true,
+//     cssEase: 'linear',
+//     arrows: false,
+//     variableWidth: true
+// });
